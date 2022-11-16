b0137153affe6ab11eb0ccbef70a09ec8f8e0b54
Svmpankaj <svmpankaj@gmail.com>
2022-11-16 11:43:45 +0530

    added leaderboard page

---
+++ src/components/styled/Leaderboard.styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const StyledLeaderboard = styled.div`
+    width: 100%;
+    margin-left: 40px;
+
+    h1 {
+        // padding: 20px;
+        margin-bottom: 40px;
+    }
+
+`
+
+export const StyledTable = styled.table`
+    tr {
+        // width: 27%;
+        // margin-left: 20px;
+        text-align: left;
+    }
+
+    th {
+        width: 27%;
+        // margin: 40px;
+        font-size: 24px
+    }
+
+    td {
+        width: 27%;
+        margin: 40px;
+
+    }
+
+`
+export const StyledHr = styled.hr`
+    width: 380%;
+    margin: 10px 10px 10px 0px;
+    padding-left: 40px;
+`
