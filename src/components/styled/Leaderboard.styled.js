d40ebc1a3483355e52eacfa926cd482518ef5445
Chaytan Arora <chaytanarora@gmail.com>
2022-11-22 20:59:41 +0530

    corrected styles in leaderboard select quiz page

--- src/components/styled/Leaderboard.styled.js
+++ src/components/styled/Leaderboard.styled.js
@@ -1,38 +1,23 @@
 import styled from "styled-components";
 
-export const StyledLeaderboard = styled.div`
-    width: 100%;
-    margin-left: 40px;
-
-    h1 {
-        // padding: 20px;
-        margin-bottom: 40px;
-    }
-
-`
-
 export const StyledTable = styled.table`
-    tr {
-        // width: 27%;
-        // margin-left: 20px;
-        text-align: left;
-    }
+	margin-top: 50px;
+	width: 70%;
 
-    th {
-        width: 27%;
-        // margin: 40px;
-        font-size: 24px
-    }
+	th {
+		padding: 20px 0;
+		/* text-align: left; */
+		font-size: 24px;
+		border-bottom: 1px solid ${({ theme }) => theme.colors.lightGreen};
+	}
 
-    td {
-        width: 27%;
-        margin: 40px;
+	td {
+		text-align: center;
+		border-bottom: 1px solid ${({ theme }) => theme.colors.lightGreen};
+		padding: 10px 0;
+	}
 
-    }
-
-`
-export const StyledHr = styled.hr`
-    width: 380%;
-    margin: 10px 10px 10px 0px;
-    padding-left: 40px;
-`
+	a {
+		text-decoration: none;
+	}
+`;
